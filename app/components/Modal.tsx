5eeacdf2f9181dbdb439e26e5494570e9bf2c87d
ilham <ibouaik@gmail.com>
2023-04-25 17:11:36 +0100

    add content and header to modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,36 +1,69 @@
-'use client'
+"use client";
 
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 interface ModalProps {
-   isOpen?: boolean;
-   onClose: () => void;
-   onSubmit: () => void;
-   title?: string;
-   body?: React.ReactElement;
-   footer?: React.ReactElement;
-   actionLabel: string;
-   disabled?: boolean;
-   secondaryAction?: () => void;
-   secondaryLabel?: string;
-
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
 }
 const Modal: React.FC<ModalProps> = ({
-   isOpen,
-   onClose,
-   onSubmit,
-   title,
-   body,
-   footer,
-   actionLabel,
-   disabled,
-   secondaryAction,
-   secondaryLabel, 
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
 }) => {
-   const [showModal, setShowModal] = useState(false)
-    return ( 
-        <>
-        <div className="flex 
+  const [showModal, setShowModal] = useState(isOpen);
+  useEffect(() => {}, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <>
+      <div
+        className="flex 
                         justify-center
                         items-center
                         overflow-x-hidden
@@ -40,8 +73,10 @@ const Modal: React.FC<ModalProps> = ({
                         z-20
                         outline-none
                         focus:outline-none
-                        bg-neutral-800/70">
-            <div className="relative
+                        bg-neutral-800/70"
+      >
+        <div
+          className="relative
                             w-full
                             md:w-4/6
                             lg:w-3/6
@@ -50,16 +85,61 @@ const Modal: React.FC<ModalProps> = ({
                             mx-auto
                             h-full
                             lg:h-auto
-                            md:h-auto">
-                {/* Content                */}
-                <div className={`translate duration-300 h-full`}>
-
-                </div>
-             </div>
+                            md:h-auto"
+        >
+          {/* Content */}
+          <div
+            className={`translate duration-300 h-full 
+                                 ${
+                                   showModal
+                                     ? "translate-y-0"
+                                     : "translate-y-full"
+                                 }
+                                 ${showModal ? "opacity-100" : "opacity-0"}`}
+          >
+            <div
+              className="translate
+                                  h-full
+                                  lg:h-auto
+                                  md:h-auto
+                                  border-0
+                                  rounded-lg
+                                  relative
+                                  flex
+                                  flex-col
+                                  w-full
+                                  bg-white
+                                  outline-none
+                                  focus:outline-none"
+            >
+              {/* Header */}
+              <div
+                className="flex
+                                     items-center
+                                     p-6
+                                     rounded-t
+                                     justify-center
+                                     relative
+                                     border-b-[1px]"
+              >
+                <button
+                  onClick={handleClose}
+                  className="p-1
+                                           border-0
+                                           hover:opacity-70
+                                           transition
+                                           absolute
+                                           left-9"
+                >
+                  <IoMdClose size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        </>
-     );
-}
- 
-export default Modal
-;
+      </div>
+    </>
+  );
+};
+
+export default Modal;
