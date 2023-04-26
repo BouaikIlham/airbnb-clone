6230c61f05f4a6f79964fff10d2070df046a4eea
ilham <ibouaik@gmail.com>
2023-04-26 22:24:49 +0100

    registerModal ui

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,8 +4,9 @@ import axios from "axios";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import Button from "../Button";
 import { toast } from "react-hot-toast";
-import { AiFillGithub } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
@@ -16,9 +17,7 @@ const RegisterModal = () => {
   const {
     register,
     handleSubmit,
-    formState: {
-      errors,
-    },
+    formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
       name: "",
@@ -34,14 +33,12 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        toast.error("This didn't work.")
+        toast.error("This didn't work.");
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
-
-
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -76,19 +73,53 @@ const RegisterModal = () => {
         required
       />
     </div>
-  )
-  return (
-    <Modal 
-        disabled= {isLoading}
-        isOpen={registerModal.isOpen}
-        title="Register"
-        actionLabel="Continue"
-        onClose={registerModal.onClose}
-        onSubmit={handleSubmit(onSubmit)}
-        body={bodyContent}
+  );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <div className="inline-flex items-center justify-center w-full">
+        <hr className="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+        <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
+          or
+        </span>
+      </div>
+      <Button
+        outline
+        label="Contine with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Contine with Facebook"
+        icon={AiFillFacebook}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row justify-center items-center gap-2">
+          <div>
+            Aleardy have an account
+          </div>
+          <div className="text-neutral-900 font-bold  cursor-pointer hover:underline"
+               onClick={registerModal.onClose}>
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
     />
-  )
+  );
 };
 
 export default RegisterModal;
