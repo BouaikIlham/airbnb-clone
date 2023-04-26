933bcf2539a70521f05eb0c2fa945ea38744d89b
ilham <ibouaik@gmail.com>
2023-04-26 14:46:57 +0100

    create form usinf hook form

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import axios from "axios";
+import Modal from "./Modal";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/UseRegisterModals";
 const RegisterModal = () => {
-    return ( 
-        <div>hhh</div>
-     );
-}
- 
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const OnSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  return (
+    <Modal 
+        disabled= {isLoading}
+        isOpen={registerModal.isOpen}
+        title="Register"
+        actionLabel="Continue"
+        onClose={registerModal.onClose}
+        onSubmit={handleSubmit(OnSubmit)}
+
+    />
+  )
+};
+
 export default RegisterModal;
