5a21fcb4ce5afca8203b1bff93cb144373723871
ilham <ibouaik@gmail.com>
2023-04-30 21:12:50 +0100

    create empty component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-
-
+import Container  from "./components/Container";
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
 export default function Home() {
+
+  const isEmpty = true
+  if(isEmpty) {
+    return (
+      <ClientOnly>
+          <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
   return (
-    <div className="bg-red-200">airbnb</div>
+    <ClientOnly>
+      <Container>
+        <div className="
+              pt-24
+              grid
+              grid-cols-1
+              sm:grid-cols-2
+              md:grid-cols-3
+              lg:grid-cols-4
+              xl:grid-cols-5
+              2xl:grid-cols-6
+              gap-8
+              ">
+        <div>My future Listing</div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
