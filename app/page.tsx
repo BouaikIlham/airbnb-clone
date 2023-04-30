6c317bf936499241a6e36520ed8ff1143d30eb1f
ilham <ibouaik@gmail.com>
2023-04-30 23:00:01 +0100

    add heartButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Container  from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";
-export default function Home() {
-
-  const isEmpty = true
-  if(isEmpty) {
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser();
+  if(listings.length === 0) {
     return (
       <ClientOnly>
           <EmptyState showReset />
@@ -25,7 +28,15 @@ export default function Home() {
               2xl:grid-cols-6
               gap-8
               ">
-        <div>My future Listing</div>
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          )
+        })}
         </div>
       </Container>
     </ClientOnly>
