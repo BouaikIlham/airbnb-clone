bc972a869f86a6e93087545045b2ec02261fb5ec
ilham <ibouaik@gmail.com>
2023-05-01 20:40:22 +0100

    fix create listing error

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import {Reservation } from "@prisma/client";
 import {SafeListing, SafeUser} from "@/app/types"
+import { categories } from "@/app/components/navbar/Categories";
+import { useMemo } from "react";
+import Container from "@/app/components/Container";
 interface ListingClientProps {
-  reservation?: Reservation;
+  reservation?: Reservation[];
   listing: SafeListing & {
     user: SafeUser
   };
@@ -12,7 +17,20 @@ const ListingClient: React.FC<ListingClientProps> = ({
     listing,
     currentUser
 }) => {
-  return <div>hello</div>;
+  const category = useMemo(() => {
+    return categories.find((item) => 
+    item.label === listing.category)
+  }, [listing.category])
+
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          {listing.title}
+        </div>
+      </div>
+    </Container>
+  )
 };
 
 export default ListingClient;
